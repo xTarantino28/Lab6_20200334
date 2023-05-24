19aede7c6c5f0197f4f1bec4373c0d0fff09cb0a
Gil Zanabria <gzanabriaz28@gmail.com>
2023-05-23 21:46:22 -0500

    p1

--- src/main/java/com/example/lab6_sol/controller/EstudianteController.java
+++ src/main/java/com/example/lab6_sol/controller/EstudianteController.java
@@ -2,13 +2,17 @@ package com.example.lab6_sol.controller;
 
 import com.example.lab6_sol.entity.Usuario;
 import com.example.lab6_sol.repository.UsuarioRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/estudiante")
@@ -23,4 +27,64 @@ public class EstudianteController {
         model.addAttribute("estudiantes", estudiantes);
         return "lista_usuarios";
     }
+
+
+    @GetMapping("/nuevoEstudiante")
+    public String nuevoEstudiante(Model model, @ModelAttribute("usuario") Usuario usuario) {
+        return "nuevoEstudiante";
+    }
+
+
+    @PostMapping("/saveEstudiante")
+    public String saveEstudiante(RedirectAttributes attr,
+                                 Model model,
+                                 @ModelAttribute("usuario") @Valid Usuario usuario,
+                                 BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()){
+            return "nuevoEstudiante";
+        }else{
+            if (usuario.getId() == 0) {
+                Optional<Usuario> usuarioOptional = usuarioRepository.findById(usuario.getId());
+                boolean existe = false;
+                if (usuarioOptional.isPresent()) {
+                    existe = true;
+                }
+                if (existe) {
+                    System.out.println("El usuario ya existe");
+                    attr.addFlashAttribute("msgDanger", "El Estudiante ya existe");
+                    return "nuevoEstudiante";
+                } else {
+                    attr.addFlashAttribute("msgGood", "Estudiante creado exitosamente");
+                    usuario.setActivo(true);
+                    usuario.setRolid(5);
+                    String passwordNOencriptada = usuario.getPassword();
+                    usuario.setPassword(new BCryptPasswordEncoder().encode(passwordNOencriptada));
+                    usuarioRepository.save(usuario);
+                    return "redirect:/estudiante/lista";
+                }
+            } else {
+                attr.addFlashAttribute("msgGood", "Estudiante actualizado exitosamente");
+                usuarioRepository.save(usuario);
+                return "redirect:/estudiante/lista";
+            }
+        }
+    }
+
+    @GetMapping("/editEstudiante")
+    public String editarEstudiante(Model model, @RequestParam("id") int id, @ModelAttribute("usuario") Usuario usuario) {
+
+        Optional<Usuario> optUsuario = usuarioRepository.findById(id);
+
+        if (optUsuario.isPresent()) {
+            usuario = optUsuario.get();
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("flagedit", "flagedit");
+            return "nuevoEstudiante";
+        } else {
+            return "redirect:/estudiante/lista";
+        }
+    }
+
+
 }
--- src/main/java/com/example/lab6_sol/entity/Usuario.java
+++ src/main/java/com/example/lab6_sol/entity/Usuario.java
@@ -1,6 +1,7 @@
 package com.example.lab6_sol.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 @Table(name = "usuario")
@@ -11,15 +12,25 @@ public class Usuario {
     @Column(nullable = false)
     private int id;
 
+    @NotBlank(message = "Es un campo obligatorio")
+    @Size(min = 3,max = 100, message = "El nombre debe ser entre 3 y 100 caracteres")
     @Column(nullable = false)
     private String nombres;
 
+    @NotBlank(message = "Es un campo obligatorio")
+    @Size(min = 3,max = 100, message = "El apellido debe ser entre 3 y 100 caracteres")
     @Column(nullable = false)
     private String apellidos;
 
+    @NotBlank(message = "Es un campo obligatorio")
+    @Size(min = 8,max = 8, message = "El dni debe ser de 8 digitos")
     @Column(nullable = false)
     private String dni;
 
+    @NotNull(message = "Es un campo obligatorio")
+    @Digits(integer= 3, fraction= 0, message = "La edad debe ser un numero entero positivo")
+    @Max(value = 120,message = "La edad debe ser un numero entero positivo no mayor a 120")
+    @Min(value = 0, message = "La edad debe ser un numero entero positivo no mayor a 120")
     @Column(nullable = false)
     private int edad;
 
